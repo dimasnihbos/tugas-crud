4408f1bef706d8debfb3ae7e9184e830fb21037c
dimasnihbos <90371402+dimasnihbos@users.noreply.github.com>
2022-10-22 11:49:10 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+   //koneksi
+   $koneksi = new PDO("mysql:host=localhost;dbname=11pplg", "root", "");
+   //query
+   //memasukan data
+      //$koneksi->query("INSERT INTO siswa(id, nama)VALUES ('3','emde')");
+   //mengedit / update data
+      //$koneksi->query("UPDATE siswa SET id='0' WHERE nama='wawan'");
+   //menghapus data
+      //$koneksi->query("DELETE FROM `siswa` WHERE id='3'");
+   //menampilkan data
+     //$koneksi->query("SELECT `id=`, `nama` FROM `siswa` WHERE 1")
+
+   
+?>
